9bb5520667eabcf832aeffe1957ea03e359ff6cc
kalieaton19-lang <kalieaton19@gmail.com>
2026-05-12 19:34:12 -0700

    fix: remove final extra closing div at end of profile page JSX structure

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -500,7 +500,6 @@ export default function ProfilePage() {
                 </div>
               </div>
             </div>
-          </div>
-      </div>
-  );
+            </div>
+          );
 }
